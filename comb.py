3687753231f5fd68a0e852209aae3abde476e4d6
FirosStuart <k934277599@gmail.com>
2021-04-03 17:35:50 +0900

    Simplify codes  and modified by Python3 Style Guide

--- comb.py
+++ comb.py
@@ -1,26 +1,44 @@
 import numpy.random
+from sys import argv
 
-#BeforeMember1、BeforeMember2...前回のメンバー
-BeforeMember1, BeforeMember2 = 0, 0
-count = 0
-#Countmax...講義の回数
-Countmax = 100
-#｛メンバーの名前、回数｝
-MemberDictionary = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0}
-while(count < Countmax):
-    #CurrentMember1、CurrentMember2...今回のメンバー
-    CurrentMember1, CurrentMember2 = numpy.random.randint(1, 6, 2)
-    #上記改善必須（現在は一様分布のランダムを利用中）
-    if CurrentMember1 == CurrentMember2 or CurrentMember1 == BeforeMember1 or CurrentMember1 == BeforeMember2 or CurrentMember2 == BeforeMember1 or CurrentMember2 == BeforeMember2:
-        pass
-        #検証用↓
-        #print("false! Member1 : " + str(CurrentMember1) + " Member2 : " + str(CurrentMember2))
-    else:
-        BeforeMember1 = CurrentMember1
-        BeforeMember2 = CurrentMember2
-        MemberDictionary[str(CurrentMember1)] = MemberDictionary[str(CurrentMember1)]+1
-        MemberDictionary[str(CurrentMember2)] = MemberDictionary[str(CurrentMember2)]+1
-        count = count+1
-        print("Member1 : " + str(CurrentMember1) + " Member2 : " + str(CurrentMember2))
 
-print(MemberDictionary)
+def main():
+    """
+    Args: (from command line)
+        member_expected 　対面参加人数
+        member_total    　参加総人数
+        max_times       　ゼミ総回数
+    Return: none
+    """
+
+    member_expected, member_total, max_times = argv[1:]
+
+    # [前回の参加者1,前回の参加者2]
+    before = [0] * int(member_expected)
+
+    # {メンバーの名前:回数}
+    member_dictionary = {str(i): 0 for i in range(1, int(member_total) + 1)}
+
+    count = 0
+    while count < int(max_times):
+        # current[今回のメンバー1,今回のメンバー1]
+        # 一様分布で乱数生成
+        current = numpy.random.randint(1, 6, 2)
+
+        if all(x not in current for x in before) and len(set(current)) == 2:
+            # 前回のメンバーと重複しない時
+            # 乱数に重複がない時
+
+            before = current.copy()
+            for i in current:
+                member_dictionary[str(i)] += 1
+            print("参加者一 : " + str(current[0]) + " 参加者二 : " + str(current[1]))
+
+            count += 1
+
+    print(member_dictionary)
+
+
+if __name__ == '__main__':
+    main()
+
