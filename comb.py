0f9d84a453b015a3dc1257b1d96c77f9d1449ca3
FirosStuart <k934277599@gmail.com>
2021-04-20 23:25:19 +0900

    5C2 最適化プラン

--- comb.py
+++ comb.py
@@ -5,7 +5,8 @@ from copy import deepcopy
 import random
 
 
-def generate_comb(member_expected, member_total, max_times):
+# 初めの組み合わせを生成する関数
+def generate_comb(member_expected, member_total, member_dictionary):
     """
     Args: (from command line)
         member_expected 　対面参加人数
@@ -14,40 +15,25 @@ def generate_comb(member_expected, member_total, max_times):
     Return:
         list[ (tuple), (tuple),...]
     """
+    while True:
+        # All combinations
+        current_comb = list(combinations([i for i in range(1, member_total + 1)], member_expected))
 
-    # All combinations
-    current_comb = list(combinations([i for i in range(1, member_total + 1)], member_expected))
+        # 一度順番をランダムにする
+        random.shuffle(current_comb)
 
-    # 一回順番をランダムにする
-    random.shuffle(current_comb)
+        # 前後でかぶっている部分がないかどうかを確かめる。かぶっていない場合には Trueが返される
+        condition = check_condition(current_comb, member_dictionary)
 
-    # 最大生成数が理論組み合わせ数より小さい場合、そのまま返す
-    if max_times < len(current_comb):
-        return current_comb
-    else:
-        # 大きい場合、理論組み合わせ数の倍数かどうかを判断
-        if max_times % len(current_comb):
-            # 倍数ではない場合、余りの部分を乱数で生成し、現リストに追加
-            # 例えば
-            # 現リスト [ (3,5), (2,4) ....(1,2) ]
-            # 12回生成の場合、余りの2回を乱数で生成する [ 0, 1 ]
-            # 現リストに[ 0, 1 ]番を後ろに追加する
-            # [ (3,5), (2,4) ....(1,2), (3,5), (2,4) ]
-            # そしてもう一回順番をランダムにする
-
-            add_list = random.sample(range(member_total), max_times % len(current_comb))
-            current_comb += [current_comb[i] for i in add_list]
-            random.shuffle(current_comb)
+        if condition:
+            return current_comb
         else:
-            # 倍数の場合、倍数ごとにリストを追加する
-            times = int(max_times / len(current_comb))
-            for i in range(times-1):
-                current_comb += current_comb
-            random.shuffle(current_comb)
-    return current_comb
+            # 前後でかぶっている部分があった場合、新たな組み合わせを生成するために、計算に使われた辞書をリセット
+            member_dictionary = member_dictionary.fromkeys(member_dictionary, 0)
 
 
-def check_condition_a(current_comb, member_dictionary):
+# 組み合わせの並べ方で、前後でかぶっているものがないかどうかを確認するための関数
+def check_condition(current_comb, member_dictionary):
     """
     Args: (from command line)
         current_comb 　   生成されたリスト
@@ -67,31 +53,29 @@ def check_condition_a(current_comb, member_dictionary):
         if any(check_list.count(i) > 1 for i in check_list):
             return False
         else:
-            # 前回のメンバーと重複しない時
+            # 前回のメンバーと被っていない場合
             before = deepcopy(current)
             for i in list(current):
                 member_dictionary[str(i)] += 1
     return True
 
 
-def check_condition_b(member_dictionary):
-    """
-    Args: (from command line)
-        member_dictionary {番号:回数}の辞書
-    Return:
-        Boolean
-    """
+# 組合せを生成するのが2度目以降(max_timesが11以上のとき)の時に利用する関数
+def re_comb(member_expected, member_total, member_dictionary, current_comb, times, added):
+    while times:
+        next_comb = generate_comb(member_expected, member_total, member_dictionary)
+        check_list = list(current_comb[-1]) + list(next_comb[0])
+        if not any(check_list.count(i) > 1 for i in check_list):
+            current_comb += next_comb
+            times -= 1
 
-    # 参加回数に0が存在する場合は不正解
-    if 0 in member_dictionary.values():
-        return False
-    else:
-        # 理論組み合わせ数が回数の場合、分散が0の場合が最適解
-        values = np.fromiter(member_dictionary.values(), dtype=float)
-        if np.var(values) != 0.0:
-            return False
-        else:
-            return True
+    return current_comb
+
+
+# 全ての組合せを表示する回数
+def print_members(current_comb, max_times):
+    for i,v in enumerate(current_comb[:max_times]):
+        print("No.", i + 1, ":", v)
 
 
 def main():
@@ -109,28 +93,13 @@ def main():
     # {メンバーの名前:回数}
     member_dictionary = {str(i): 0 for i in range(1, member_total + 1)}
 
-    while True:
+    times = max_times // theory_num  # 42 // 10 = 4
+    added = max_times % theory_num  # 42 % 10 = 2
 
-        # Combinationを生成する
-        current_comb = generate_comb(member_expected, member_total, max_times)
-
-        # 状況Aに合うか
-        condition_a = check_condition_a(current_comb, member_dictionary)
-
-        # ちょうど組み合わせ理論数の場合だけ 状況Bを判断する
-        if max_times == theory_num:
-            condition_b = check_condition_b(member_dictionary)
-        else:
-            condition_b = True
-
-        # 全て合致の場合、出力
-        if condition_a and condition_b:
-            for i in current_comb[:max_times]:
-                print(i)
-            break
-        else:
-            # なければ、計算に使われた辞書をリセット
-            member_dictionary = member_dictionary.fromkeys(member_dictionary, 0)
+    # Combinationを生成する。
+    current_comb = generate_comb(member_expected, member_total, member_dictionary)
+    all_comb = re_comb(member_expected, member_total, member_dictionary, current_comb, times, added)
+    print_members(all_comb, max_times)
 
 
 if __name__ == '__main__':
