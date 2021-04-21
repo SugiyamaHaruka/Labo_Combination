0293ef8917693fe77831f067b78c17323d5659f7
FirosStuart <k934277599@gmail.com>
2021-04-21 17:19:12 +0900

    Comment Renew

--- comb.py
+++ comb.py
@@ -18,7 +18,12 @@ def generate_comb(member_expected, member_total, member_dictionary):
     while True:
         # All combinations
         current_comb = list(combinations([i for i in range(1, member_total + 1)], member_expected))
-
+        """
+        test_list = []
+        for i in range(i, member_total + 1):
+            test_list.append(i)
+        current_comb = list(combinations(test_list,member_expected))
+        """
         # 一度順番をランダムにする
         random.shuffle(current_comb)
 
@@ -50,6 +55,13 @@ def check_condition(current_comb, member_dictionary):
 
         current = current_comb[index]
         check_list = list(before) + list(current)
+        """
+        test_flag = True
+        for i in check_list:
+            if check_list.count(i) > 1:
+                test_flag = False
+                break
+        """
         if any(check_list.count(i) > 1 for i in check_list):
             return False
         else:
@@ -65,6 +77,11 @@ def re_comb(member_expected, member_total, member_dictionary, current_comb, time
     while times:
         next_comb = generate_comb(member_expected, member_total, member_dictionary)
         check_list = list(current_comb[-1]) + list(next_comb[0])
+        """
+        current_comb = (1,2)
+        next_comb = (3,4)
+        check_list = [1,2,3,4]
+        """
         if not any(check_list.count(i) > 1 for i in check_list):
             current_comb += next_comb
             times -= 1
@@ -88,11 +105,23 @@ def main():
     """
 
     member_expected, member_total, max_times = [int(i) for i in argv[1:]]
+    """
+    arg_list = []
+    for i in argv:
+        arg_list.append(i)
+    member_expected, member_total, max_times = arg_list
+    # arg_list: ["?","?","?"]
+    """
     # 組み合わせの理論数
     theory_num = len(list(combinations([i for i in range(1, member_total + 1)], member_expected)))
+    
     # {メンバーの名前:回数}
     member_dictionary = {str(i): 0 for i in range(1, member_total + 1)}
-
+    """
+    member_dictionary = {}
+    for i in range(1,member_total + 1):
+        member_dictionary[str(i)] = 0
+    """
     times = max_times // theory_num  # 42 // 10 = 4
     added = max_times % theory_num  # 42 % 10 = 2
 
